2fd6b9db164337f6843ad069de0f05e3edcfb7d8
osbornej-tech <60370291+osbornej-tech@users.noreply.github.com>
2020-11-08 21:05:50 -0500

    Add files via upload

--- Query/Employee_Database_Challenge.sql
+++ Query/Employee_Database_Challenge.sql
@@ -32,7 +32,7 @@ select count(emp_no) from uniquetitles
 -- Count the number of employees retiring by title, create a new table
 select count(title)
 	,title 
-into retiring_titles
+--into retiring_titles
 from uniquetitles
 group by title
 order by count(title) desc
@@ -68,3 +68,39 @@ order by count(title) desc
 
 
 select count(emp_no) from emp -- Number of employees = 300024
+
+--All information from the uniquetitles table where the employee is a manager
+select *, dept_no from uniquetitles
+left join dept_manager  on uniquetitles.emp_no = dept_manager.emp_no
+where title ='Manager'
+
+--Top ten highest paid employees
+select a.emp_no, title, salary from uniquetitles as a
+left join salaries on a.emp_no = salaries.emp_no
+--group by title, salary 
+order by salary desc 
+Limit 10
+
+select count(emp_no) from retirement_titles
+
+--Provide count of employee titles for individuals in the mentorship eligibilty table
+select count(e.emp_no),title from uniquetitles as b
+left join emp e on e.emp_no = b.emp_no
+where e.birth_date >='1955-01-01'
+group by title
+order by count(e.emp_no) desc
+
+
+
+
+-- Ineligible for retirement based on age
+select distinct emp.emp_no, title 
+into remaining
+from emp
+inner join titles as t on emp.emp_no = t.emp_no
+where birth_date >= '1965-01-01'
+
+
+select count(title),title from remaining
+group by title 
+order by count(title) desc
